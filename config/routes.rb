6c0d56a48acc2ee3ab855d9a1b093fd8de5d834e
Lin To <linxuanto@gmail.com>
2019-07-24 23:27:43 +0800

    added style for basic airbnb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'flats/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :flats
+  root to: 'flats#index'
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Flat.delete_all if Rails.env.development?
+
+10.times do
+  Flat.create!(name: Faker::Name.unique.name,
+                address: Faker::Address.street_address,
+                description: Faker::Artist.name,
+                price_per_night: rand(90..200),
+                number_of_guest: rand(1..10),
+    )
+
+end
+
+puts "created #{Flat.count} flats"
+
+# name, as a string
+# address, as a string
+# description, as a text
+# price_per_night, as an integer
+# number_of_guests, as an integer
